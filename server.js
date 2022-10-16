c82d1a33cc2b713b1fc3d59b90f9faa008ff6f14
higjo860 <101899879+higjo860@users.noreply.github.com>
2022-10-17 08:02:42 +1300

    Level 2 & 3

--- server.js
+++ server.js
@@ -3,8 +3,19 @@ require('dotenv').config();
 const express = require('express');
 const myDB = require('./connection');
 const fccTesting = require('./freeCodeCamp/fcctesting.js');
+const session = require('express-session');
+const passport = require('passport');
 
 const app = express();
+
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: true,
+  saveUninitialized: true,
+  cookie: {secure: false}
+}))
+app.use(passport.initialize());
+app.use(passport.session());
 
 
 app.set('view engine', 'pug')
@@ -18,7 +29,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.route('/').get((req, res) => {
 
-  res.render(process.cwd() + '/views/pug')
+  res.render(process.cwd() + '/views/pug', {title: 'Hello', message: 'Please login'})
 });
 
 const PORT = process.env.PORT || 3000;
